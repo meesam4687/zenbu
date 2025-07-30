287d071d69227b5d9ffb6dd2c3595b39d0de49f8
meesam4687 <meesam4687@gmail.com>
2025-07-30 15:06:47 +0530

    ui: increase appbar height and add a slight border to the image for better visibility

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,19 +7,28 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        toolbarHeight: 60,
         title: const Text("Home"),
         actions: [
           Padding(
             padding: const EdgeInsets.only(right: 5),
             child: IconButton(
               onPressed: () {},
-              icon: ClipOval(
-                child: Image(
-                  height: 40,
-                  width: 40,
-                  fit: BoxFit.fill,
-                  image: NetworkImage(
-                    "https://fujiframe.com/assets/images/_3000x2000_fit_center-center_85_none/10085/xhs2-fuji-70-300-Amazilia-Hummingbird.webp",
+              icon: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Theme.of(context).colorScheme.onSecondary,
+                  ),
+                  borderRadius: BorderRadius.all(Radius.circular(360)),
+                ),
+                child: ClipOval(
+                  child: Image(
+                    height: 40,
+                    width: 40,
+                    fit: BoxFit.fill,
+                    image: NetworkImage(
+                      "https://fujiframe.com/assets/images/_3000x2000_fit_center-center_85_none/10085/xhs2-fuji-70-300-Amazilia-Hummingbird.webp",
+                    ),
                   ),
                 ),
               ),
