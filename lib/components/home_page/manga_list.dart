fd3bdcd6caa6a765e8965398cce602dccd432f4f
meesam4687 <meesam4687@gmail.com>
2025-08-01 11:12:19 +0530

    feat: make mangalist use listview builder as well

--- lib/components/home_page/manga_list.dart
+++ lib/components/home_page/manga_list.dart
@@ -6,8 +6,41 @@ class MangaList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<Map<String, String>> items = [
+      {
+        "title": "Ligma the First",
+        "image":
+            "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx177937-HDZQqDqXQqxs.jpg",
+        "state": "2/23",
+      },
+      {
+        "title": "Ligma the Second",
+        "image":
+            "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx177686-9CwCvp3mkDI3.jpg",
+        "state": "3/12",
+      },
+      {
+        "title": "Ligma the Third",
+        "image":
+            "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx132776-U34wk1Zbx7pU.png",
+        "state": "5/11",
+      },
+      {
+        "title": "Ligma the Fourth",
+        "image":
+            "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx186052-zq8h0Qo0O0sP.jpg",
+        "state": "7/12",
+      },
+      {
+        "title": "Ligma the Fifth",
+        "image":
+            "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx196063-hDLoZ2anG97G.png",
+        "state": "8/13",
+      },
+    ];
+
     return Container(
-      margin: EdgeInsets.only(top: 0),
+      margin: EdgeInsets.only(top: 10),
       width: double.infinity,
       height: 320,
       child: Column(
@@ -44,36 +77,17 @@ class MangaList extends StatelessWidget {
             width: double.infinity,
             height: 260,
             margin: EdgeInsets.only(left: 12, right: 12),
-            child: SingleChildScrollView(
+            child: ListView.builder(
               scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  ItemCard(
-                    title: "Ligma Balls the First",
-                    image:
-                        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx177937-HDZQqDqXQqxs.jpg",
-                    state: "1/20",
-                  ),
-                  ItemCard(
-                    title: "Ligma Balls the Second",
-                    image:
-                        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx140291-JJnzSWCKSuPS.jpg",
-                    state: "1/20",
-                  ),
-                  ItemCard(
-                    title: "Ligma Balls the Third",
-                    image:
-                        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx180523-FjvW1XByUNMK.jpg",
-                    state: "1/20",
-                  ),
-                  ItemCard(
-                    title: "Ligma Balls the Fourth",
-                    image:
-                        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx184322-TNA26Kg1I7Bd.jpg",
-                    state: "1/20",
-                  ),
-                ],
-              ),
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                final item = items[index];
+                return ItemCard(
+                  title: item["title"] as String,
+                  state: item["state"] as String,
+                  image: item["image"] as String,
+                );
+              },
             ),
           ),
         ],
