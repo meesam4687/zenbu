2e6f9ad82046db32243e1816dbde1696c9d97268
meesam4687 <meesam4687@gmail.com>
2025-08-17 12:18:49 +0530

    chore: move and rename banner widget

---
+++ lib/components/global/banner.dart
@@ -0,0 +1,156 @@
+import 'dart:ui' as ui;
+import 'package:al_client/pages/anime_details_page.dart';
+import 'package:al_client/pages/character_details_page.dart';
+import 'package:al_client/pages/manga_details_page.dart';
+import 'package:animations/animations.dart';
+import 'package:flutter/material.dart';
+
+class AiringBanner extends StatelessWidget {
+  const AiringBanner({
+    super.key,
+    required this.id,
+    required this.bannerImage,
+    required this.coverImage,
+    required this.title,
+    required this.totalEpisodes,
+    required this.airedEpisodes,
+    required this.tagString,
+    required this.type,
+  });
+
+  final int id;
+  final String bannerImage;
+  final String coverImage;
+  final String title;
+  final String totalEpisodes;
+  final String airedEpisodes;
+  final String tagString;
+  final String type;
+
+  @override
+  Widget build(BuildContext context) {
+    final double cardRadius = 15;
+    return OpenContainer(
+      openElevation: 0,
+      closedElevation: 0,
+      transitionType: ContainerTransitionType.fadeThrough,
+      openColor: Theme.of(context).colorScheme.surface,
+      closedColor: Theme.of(context).colorScheme.surface,
+      closedShape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      closedBuilder: (context, openContainer) {
+        return Container(
+          height: 240,
+          width: double.infinity,
+          margin: EdgeInsets.all(10),
+          child: Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(cardRadius)),
+            ),
+            elevation: 3,
+            surfaceTintColor: Theme.of(context).colorScheme.onSurface,
+            clipBehavior: Clip.antiAlias,
+            child: Stack(
+              children: [
+                Positioned.fill(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(cardRadius),
+                    child: ImageFiltered(
+                      imageFilter: ui.ImageFilter.blur(sigmaX: 15, sigmaY: 15),
+                      child: Image.network(
+                        bannerImage,
+                        fit: BoxFit.cover,
+                        errorBuilder: (context, error, stackTrace) =>
+                            const Icon(Icons.error),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(color: Colors.black.withValues(alpha: 0.4)),
+                Container(
+                  margin: const EdgeInsets.all(10),
+                  height: 240,
+                  width: double.infinity,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        height: 190,
+                        width: 140,
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(14)),
+                          ),
+                          elevation: 5,
+                          clipBehavior: Clip.antiAlias,
+                          child: Image.network(
+                            coverImage,
+                            fit: BoxFit.cover,
+
+                            errorBuilder: (context, error, stackTrace) =>
+                                const Icon(Icons.error),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Text(
+                              title,
+                              style: TextStyle(
+                                fontSize: 24,
+                                shadows: [
+                                  BoxShadow(
+                                    blurRadius: 5.0,
+                                    color: Colors.black,
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Text(
+                              "Episodes: $airedEpisodes/$totalEpisodes\n$tagString",
+                              style: TextStyle(
+                                fontSize: 15,
+                                shadows: [
+                                  BoxShadow(
+                                    blurRadius: 5.0,
+                                    color: Colors.black,
+                                  ),
+                                ],
+                              ),
+                            ),
+                            SizedBox(height: 18),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Positioned.fill(
+                  child: Material(
+                    color: Colors.transparent,
+                    child: InkWell(onTap: openContainer),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+      openBuilder: (context, closeContainer) {
+        if (type == "anime") {
+          return AnimeDetailsPage(id: id);
+        } else if (type == "manga") {
+          return MangaDetailsPage(id: id);
+        } else if (type == "character") {
+          return CharacterDetailsPage(id: id);
+        }
+        return Placeholder();
+      },
+    );
+  }
+}
